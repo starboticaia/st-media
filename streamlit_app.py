84485811670029c46cea38f25d5158b04342c978
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 17:01:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,17 @@ import streamlit as st
 urlImagen = 'https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG'
 urlVideo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D'
 
+# uno debajo de otro
 st.image(urlImagen, caption='nyan cat')
 st.video(urlVideo)
+
+# estructuramos mejor
+col1, col2 = st.columns(2)
+
+with col1:
+  st.write('Imagen')
+  st.image(urlImagen, caption='nyan cat')
+
+with col2:
+  st.write('Video')
+  st.video(urlVideo)
