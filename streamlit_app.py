9238bca08450382bfb8852193d9febdd7482c2be
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 17:03:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit as st
 urlImagen = 'https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG'
 urlVideo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D'
 
-# uno debajo de otro
-st.image(urlImagen, caption='nyan cat')
-st.video(urlVideo)
+# uno debajo de otro, dentro de un expansor
+with st.expander('Ver detalles...'):
+  st.image(urlImagen, caption='nyan cat')
+  st.video(urlVideo)
 
 # estructuramos mejor
 col1, col2 = st.columns(2)
