ccb51b5f4d4d7173ec7223411e1f89a2f0cac51f
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 16:58:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 urlImagen = 'https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG'
-urlVideo = https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D
+urlVideo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D'
 
 st.image(urlImagen, caption='nyan cat')
 st.video(urlVideo)
